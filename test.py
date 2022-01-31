2adde9fc4220e7632c1ca725004ad0413c6ac7d1
jun117 <kbokro@icloud.com>
2022-01-31 11:38:43 +0900

    関数化をtestファイル、 してないhackerファイル
    とりあえず出力はできたがエラーあり

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
 import requests
 
-topstories = "8863"
-url = f"https://hacker-news.firebaseio.com/v0/item/{topstories}.json"
 
-response = requests.get(url)
+def search_url_list():
+    url_list = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    response_list = requests.get(url_list)
+    dic_lists = response_list.json()
+    return dic_lists
 
-dic = response.json()
-news_title = dic["title"]
-news_link = dic["url"]
-print(f"'title':'{news_title}','link':'{news_link}'")
 
-"""
-print((response))
-print(response.json()['id'])
-print(type(response.json()))
-"""
+def search_news(dic_list):
+    url = f"https://hacker-news.firebaseio.com/v0/item/{dic_list}.json"
+    response = requests.get(url)
+    dic = response.json()
+    news_title = dic["title"]
+    news_link = dic["url"]
+    results = f"'title':'{news_title}','link':'{news_link}'"
+    return results
+
+
+def main():
+    dic_lists = search_url_list()
+
+    for dic_list in dic_lists:
+        results = search_news(dic_list)
+
+        print(results)
+
+
+if __name__ == '__main__':
+    main()
