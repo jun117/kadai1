9d1458284537e82378282a3e17cf50ffd9c8b27e
jun117 <kbokro@icloud.com>
2022-02-01 08:37:04 +0900

    出力はできた
    関数化はできていないhackerファイル

--- hacker.py
+++ hacker.py
@@ -1,22 +1,28 @@
 import requests
 import time
 
-url_list = "https://hacker-news.firebaseio.com/v0/topstories.json"
-response_list = requests.get(url_list)
 
-dic_lists = response_list.json()
+def main():
+    url_list = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    response_list = requests.get(url_list)
 
-for dic_list in dic_lists:
-    url_text = f"https://hacker-news.firebaseio.com/v0/item/{dic_list}.json"
+    dic_lists = response_list.json()
 
-    response = requests.get(url_text)
+    for dic_list in dic_lists:
+        url_text = f"https://hacker-news.firebaseio.com/v0/item/{dic_list}.json"
 
-    dic = response.json()
+        response = requests.get(url_text)
 
-    if 'url' in dic:
-        news_title = dic['title']
-        news_link = dic['url']
-        for i in range(10):
-            time.sleep(1)
+        dic = response.json()
 
-        print(f"'title':'{news_title}','link':'{news_link})'")
+        if 'url' in dic:
+            news_title = dic['title']
+            news_link = dic['url']
+            for i in range(3):
+                time.sleep(1)
+
+            print(f"'title':'{news_title}','link':'{news_link})'")
+
+
+if __name__ == '__main__':
+    main()
